857b6541f3e73c405c2d81f967dcbcbf94e34d06
Olivier <biganiroo.com>
2024-04-18 14:30:47 +0200

    project

--- finalYearProject/routes/web.php
+++ finalYearProject/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\student\chatcontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,4 +25,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/student/chat', [chatcontroller::class, 'index'])->name('student.chat');
 });
